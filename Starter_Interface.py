68f8fb61c81f0030a0d3ec4fd64d04fa1930b120
TRECAM06 <cameron7640@live.missouristate.edu>
2022-09-25 01:17:51 -0500

    flipped camera for finger tracking to screen Added comments on main components Commented out old parts that were really unnecessary including the for loop FPS counter, and camera display

--- Starter_Interface.py
+++ Starter_Interface.py
@@ -2,37 +2,50 @@ import pyautogui
 import cv2
 import mediapipe as mp
 import time
-wCam, hCam = 1920,1080
 
+#Width and Height of Monitor
+wCam, hCam = pyautogui.size()[0], pyautogui.size()[1]
+
+#Initialize Video Capture
 cap = cv2.VideoCapture(0)
 
+#Set a Aspect Ratio Scale to capture frame (keeps the viewport a nice manageable size)
 cap.set(16, wCam)
 cap.set(9, hCam)
 
-
+#Initialize Hands from MP and mpdraw for showing landmarks
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
 mpdraw = mp.solutions.drawing_utils
+
 ptime = 0
 while True:
+    #Capture image must write to success and img for the capture due to OpenCV parameter requirements
     success, img = cap.read()
+    #flip
     img = cv2.flip(img, 1)
+    #img must be converted to RGB to be read by mediapipe
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #This reads the image from the camera and detects hands, turing them into objects
     results = hands.process(imgRGB)
     #print(results.multi_hand_landmarks)
 
+    #get hand, for each landmark in result draw the landmark and record the xl,yl of the indexfinger tip
     if results.multi_hand_landmarks:
-        for handlms in results.multi_hand_landmarks:
-            mpdraw.draw_landmarks(img, handlms)
-            xl = handlms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * wCam
-            yl = handlms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * hCam
-            
-            pyautogui.moveTo(xl,yl)
-    ctime = time.time()
-    fps = 1/(ctime-ptime)
-    ptime=ctime
-    cv2.putText(img, f"FPS: {int(fps)}", (40,50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 2)
-    cv2.imshow("Img", img)
-    cv2.waitKey(1)
+        xl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * wCam
+        yl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * hCam
+        #for handlms in results.multi_hand_landmarks:
+            #mpdraw.draw_landmarks(img, handlms)
+            #print(handlms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP])
+            #xl = handlms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * wCam
+            #yl = handlms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * hCam
+
+        pyautogui.moveTo(xl,yl)
+    #ctime = time.time()
+    #fps = 1/(ctime-ptime)
+    #ptime=ctime
+    #cv2.putText(img, f"FPS: {int(fps)}", (40,50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 2)
+    #cv2.imshow("Img", img)
+    #cv2.waitKey(1)
     
     
