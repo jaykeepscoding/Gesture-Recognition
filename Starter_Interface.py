ebed4ecf016ec55e466d113b23ea53b721614f87
TRECAM06 <cameron7640@live.missouristate.edu>
2022-09-26 13:18:39 -0500

    Updated Necessities, removed commented parts of loop for readability.

--- Starter_Interface.py
+++ Starter_Interface.py
@@ -32,20 +32,10 @@ while True:
 
     #get hand, for each landmark in result draw the landmark and record the xl,yl of the indexfinger tip
     if results.multi_hand_landmarks:
+
         xl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * wCam
         yl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * hCam
-        #for handlms in results.multi_hand_landmarks:
-            #mpdraw.draw_landmarks(img, handlms)
-            #print(handlms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP])
-            #xl = handlms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * wCam
-            #yl = handlms.landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * hCam
 
         pyautogui.moveTo(xl,yl)
-    #ctime = time.time()
-    #fps = 1/(ctime-ptime)
-    #ptime=ctime
-    #cv2.putText(img, f"FPS: {int(fps)}", (40,50), cv2.FONT_HERSHEY_COMPLEX, 1, (255,0,0), 2)
-    #cv2.imshow("Img", img)
-    #cv2.waitKey(1)
     
     
