83695f2c79de73d7032a0d566d47b1b8f40511cb
TRECAM06 <cameron7640@live.missouristate.edu>
2022-09-29 20:10:36 -0500

    Introduced keyboard module Added end program if CTRL is pressed.

--- Starter_Interface.py
+++ Starter_Interface.py
@@ -2,7 +2,7 @@ import pyautogui
 import cv2
 import mediapipe as mp
 import time
-
+import keyboard
 #Width and Height of Monitor
 wCam, hCam = pyautogui.size()[0], pyautogui.size()[1]
 
@@ -30,11 +30,16 @@ while True:
 
     #get hand, for each landmark in result draw the landmark and record the xl,yl of the indexfinger tip
     if results.multi_hand_landmarks:
+        if keyboard.is_pressed('Ctrl'):
+            exit()
 
-        time.sleep(.05)
         xl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * wCam
         yl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * hCam
         print(xl,yl)
         pyautogui.moveTo(xl,yl)
+
+
+
+
 
     
