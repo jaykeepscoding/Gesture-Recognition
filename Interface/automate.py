77a18cf5f0c960e893fbdedce0ff7d2ecd5561a4
TRECAM06 <77004332+TRECAM06@users.noreply.github.com>
2022-10-19 17:02:14 -0500

    Add files via upload

---
+++ Interface/automate.py
@@ -0,0 +1,51 @@
+
+
+import tkinter
+from tkinter import *
+from PIL import Image, ImageTk
+import cv2
+import time
+import pyautogui
+import mediapipe as mp
+import keyboard
+import pyautogui
+
+
+wCam, hCam = pyautogui.size()[0], pyautogui.size()[1]
+
+#Initialize Video Capture
+cap = cv2.VideoCapture(0)
+
+#Set a Aspect Ratio Scale to capture frame (keeps the viewport a nice manageable size)
+cap.set(16, wCam)
+cap.set(9, hCam)
+
+#Initialize Hands from MP and mpdraw for showing landmarks
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpdraw = mp.solutions.drawing_utils
+
+
+while True:
+    #Capture image must write to success and img for the capture due to OpenCV parameter requirements
+    success, img = cap.read()
+    #flip
+    img = cv2.flip(img, 1)
+    #img must be converted to RGB to be read by mediapipe
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #This reads the image from the camera and detects hands, turing them into objects
+    results = hands.process(imgRGB)
+
+    #get hand, for each landmark in result draw the landmark and record the xl,yl of the indexfinger tip
+    if results.multi_hand_landmarks:
+        if keyboard.is_pressed('Ctrl'):
+            exit()
+        fingers = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
+        for v in fingers:
+            xl = results.multi_hand_landmarks[0].landmark[v].x * wCam
+            yl = results.multi_hand_landmarks[0].landmark[v].y * hCam
+            print(v, xl, yl)
+
+            
+        #print(xl,yl)
+
