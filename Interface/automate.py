ced2edc103f4f8f0dc31ef996995dd004436f821
TRECAM06 <cameron7640@live.missouristate.edu>
2022-10-20 13:27:42 -0500

    Program now also outputs normalized positions in same (x,y) fashion below pixel points

--- Interface/automate.py
+++ Interface/automate.py
@@ -34,7 +34,39 @@ def createList(aimage):
             yl = results.multi_hand_landmarks[0].landmark[v].y * hCam
             #print(v, xl, yl)
             if v in lst_hd:
+                newp[str(v)] = (xl,yl)
 
+    print(newp)
+
+    return newp
+
+def createNormList(aimage):
+    #wCam, hCam = pyautogui.size()[0], pyautogui.size()[1]
+
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpdraw = mp.solutions.drawing_utils
+
+    comp_lst = [[],[],[],[],[]]
+    lst_hd = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
+    newp = {}
+
+    img = cv2.imread(aimage,0)
+
+    img = cv2.flip(img, 1)
+    wCam = img.shape[0]
+    hCam = img.shape[1]
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        fingers = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
+        for v in fingers:
+            xl = results.multi_hand_landmarks[0].landmark[v].x
+            yl = results.multi_hand_landmarks[0].landmark[v].y
+            #print(v, xl, yl)
+            if v in lst_hd:
                 newp[str(v)] = (xl,yl)
 
     print(newp)
@@ -54,6 +86,7 @@ with open('imagedata.csv', 'w', newline='') as csvfile:
     for x in images:
             print(x)
             tst = createList(x)
+            tst2 = createNormList(x)
             print(type(tst), len(tst))
             if len(tst) != 0:
                 csv_writer.writeheader()
@@ -61,5 +94,6 @@ with open('imagedata.csv', 'w', newline='') as csvfile:
 
                         #csv_writer.writerow((str(tst[0][y])[13:][:-4]))
                 csv_writer.writerow(tst)
+                csv_writer.writerow(tst2)
             else:
                 pass
