a140b0346910550d036d8a4616ef200d75396328
TRECAM06 <cameron7640@live.missouristate.edu>
2022-10-20 14:08:01 -0500

    Lowered detection confidence for image data, too many images weren't detecting the hand. This may affect usefulness of the data. Not really sure by how much.

--- Interface/automate.py
+++ Interface/automate.py
@@ -17,26 +17,24 @@ def createList(aimage):
     comp_lst = [[],[],[],[],[]]
     lst_hd = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
     newp = {}
-
     img = cv2.imread(aimage,0)
+    cv2.imshow("", img)
+    cv2.waitKey(0)
 
-    img = cv2.flip(img, 1)
+    #img = cv2.flip(img, 1)
     wCam = img.shape[0]
     hCam = img.shape[1]
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-
-    results = hands.process(imgRGB)
-
-    if results.multi_hand_landmarks:
-        fingers = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
-        for v in fingers:
-            xl = results.multi_hand_landmarks[0].landmark[v].x * wCam
-            yl = results.multi_hand_landmarks[0].landmark[v].y * hCam
-            #print(v, xl, yl)
-            if v in lst_hd:
-                newp[str(v)] = (xl,yl)
-
-    print(newp)
+    with mpHands.Hands(min_detection_confidence=0.001, min_tracking_confidence=.01) as hands:
+        results = hands.process(imgRGB)
+        if results.multi_hand_landmarks:
+            fingers = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
+            for v in fingers:
+                xl = results.multi_hand_landmarks[0].landmark[v].x * wCam
+                yl = results.multi_hand_landmarks[0].landmark[v].y * hCam
+                #print(v, xl, yl)
+                if v in lst_hd:
+                    newp[str(v)] = (xl,yl)
 
     return newp
 
@@ -46,28 +44,24 @@ def createNormList(aimage):
     mpHands = mp.solutions.hands
     hands = mpHands.Hands()
     mpdraw = mp.solutions.drawing_utils
-
-    comp_lst = [[],[],[],[],[]]
     lst_hd = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
     newp = {}
 
     img = cv2.imread(aimage,0)
 
-    img = cv2.flip(img, 1)
-    wCam = img.shape[0]
-    hCam = img.shape[1]
+    #img = cv2.flip(img, 1)
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    with mpHands.Hands(min_detection_confidence=0.001, min_tracking_confidence=.01) as hands:
+        results = hands.process(imgRGB)
 
-    results = hands.process(imgRGB)
-
-    if results.multi_hand_landmarks:
-        fingers = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
-        for v in fingers:
-            xl = results.multi_hand_landmarks[0].landmark[v].x
-            yl = results.multi_hand_landmarks[0].landmark[v].y
-            #print(v, xl, yl)
-            if v in lst_hd:
-                newp[str(v)] = (xl,yl)
+        if results.multi_hand_landmarks:
+            fingers = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
+            for v in fingers:
+                xl = results.multi_hand_landmarks[0].landmark[v].x
+                yl = results.multi_hand_landmarks[0].landmark[v].y
+                #print(v, xl, yl)
+                if v in lst_hd:
+                    newp[str(v)] = (xl,yl)
 
     print(newp)
 
@@ -89,7 +83,7 @@ with open(folderRes, 'w', newline='') as csvfile:
             print(x)
             tst = createList(x)
             tst2 = createNormList(x)
-            print(type(tst), len(tst))
+            print(type(tst), len(tst), tst)
             if len(tst) != 0:
                 csv_writer.writeheader()
 
