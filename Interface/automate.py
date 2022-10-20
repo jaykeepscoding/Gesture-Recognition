c46eac5b6251c58622f073b2419f922dd2665889
TRECAM06 <cameron7640@live.missouristate.edu>
2022-10-20 01:11:43 -0500

    This program takes images in the "images" directory and parses the positions of the tips of each finger in (x,y) tuples, currently only gives pixel data.

--- Interface/automate.py
+++ Interface/automate.py
@@ -4,47 +4,55 @@ import time
 import mediapipe as mp
 import pyautogui
 import csv
-info = open("Interface/tst.csv", "rw+")
-wCam, hCam = pyautogui.size()[0], pyautogui.size()[1]
+import glob, os
 
-#Initialize Video Capture
-#cap = cv2.VideoCapture(0)
-#Set a Aspect Ratio Scale to capture frame (keeps the viewport a nice manageable size)
-#cap.set(16, wCam)
-#cap.set(9, hCam)
+#info = open("Interface/tst.csv", "rw+")
+def createList(aimage):
+    wCam, hCam = pyautogui.size()[0], pyautogui.size()[1]
+    mpHands = mp.solutions.hands
+    hands = mpHands.Hands()
+    mpdraw = mp.solutions.drawing_utils
 
-#Initialize Hands from MP and mpdraw for showing landmarks
-mpHands = mp.solutions.hands
-hands = mpHands.Hands()
-mpdraw = mp.solutions.drawing_utils
+    comp_lst = [[],[],[],[],[]]
+    lst_hd = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
+    newp = {}
 
-comp_lst = [[],[],[],[],[]]
-lst_hd = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
+    img = cv2.imread(aimage,0)
 
-#Capture image must write to success and img for the capture due to OpenCV parameter requirements
-#success, img = cap.read()
-img = cv2.imread('Interface/tstmg.jpg',0)
-#flip
-img = cv2.flip(img, 1)
-#img must be converted to RGB to be read by mediapipe
-imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-#This reads the image from the camera and detects hands, turing them into objects
-results = hands.process(imgRGB)
+    img = cv2.flip(img, 1)
 
-#get hand, for each landmark in result draw the landmark and record the xl,yl of the indexfinger tip
-if results.multi_hand_landmarks:
-    fingers = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
-    for v in fingers:
-        xl = results.multi_hand_landmarks[0].landmark[v].x * wCam
-        yl = results.multi_hand_landmarks[0].landmark[v].y * hCam
-        #print(v, xl, yl)
-        if v in lst_hd:
-            rt = lst_hd.index(v)
-            comp_lst[rt] = (xl,yl)
-    print(lst_hd)
-    print(comp_lst)
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        fingers = [mpHands.HandLandmark.THUMB_TIP, mpHands.HandLandmark.INDEX_FINGER_TIP, mpHands.HandLandmark.MIDDLE_FINGER_TIP, mpHands.HandLandmark.RING_FINGER_TIP, mpHands.HandLandmark.PINKY_TIP]
+        for v in fingers:
+            xl = results.multi_hand_landmarks[0].landmark[v].x * wCam
+            yl = results.multi_hand_landmarks[0].landmark[v].y * hCam
+            #print(v, xl, yl)
+            if v in lst_hd:
+
+                newp[str(v)] = (xl,yl)
+
+    print(newp)
+
+    return newp
+images = []
+for root, dirs, files in os.walk("images"):
+    for file in files:
+        if file.endswith(".jpg"):
+            images.append(os.path.join(root,file))
+
+with open('imagedata.csv', 'w', newline='') as csvfile:
+    fieldnames = ['HandLandmark.RING_FINGER_TIP','HandLandmark.THUMB_TIP','HandLandmark.PINKY_TIP','HandLandmark.INDEX_FINGER_TIP','HandLandmark.MIDDLE_FINGER_TIP']
+
+    csv_writer = csv.DictWriter(csvfile,fieldnames=fieldnames)
+    #spamwriter.writerow("THUMB INDEX MIDDLE RING PINKY")
+    for x in images:
+        tst = createList(x)
+        csv_writer.writeheader()
 
 
-
-    #print(xl,yl)
-
+            #csv_writer.writerow((str(tst[0][y])[13:][:-4]))
+        csv_writer.writerow(tst)
