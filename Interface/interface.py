df38f06bc97d75d9e342007dd91fcb6aee3efb64
TRECAM06 <cameron7640@live.missouristate.edu>
2022-10-25 11:50:35 -0500

    Allows users to start and stop feed. Removed annoying Tkinter Logo Gave it a title Will probably add a feature to toggle-off Hand Tracking

--- Interface/interface.py
+++ Interface/interface.py
@@ -7,13 +7,20 @@ import pyautogui
 import mediapipe as mp
 import keyboard
 import pyautogui
+import tkinter.ttk as ttk
+from tkinter.ttk import *
+from ttkthemes import ThemedTk
 class Interface():
    def __init__(self):
       self.win = Tk()
-      #Operations(self.win)
-      optics_inst = opticsControl(self.win)
-
-      tkinter.Button(self.win, text="Kill feed", command=optics_inst.setCamera).pack()
+      s = ttk.Style()
+      print(s.theme_names())
+      s.theme_use('xpnative')
+      self.win.title("Gesture Interface")
+      self.win.wm_attributes('-toolwindow', 'True')
+      self.win.wm_attributes('-topmost', 'True')
+      self.win.resizable(0,0)
+      opticsControl(self.win)
       self.win.mainloop()
 
 
@@ -27,8 +34,7 @@ class Operations():
       print("A")
 
 
-   def killFeed(self):
-      return 0
+
    def exit(self):
       return 0
    def reset(self):
@@ -36,53 +42,55 @@ class Operations():
 
 class opticsControl():
    def __init__(self, win):
+      self.live = 1
       self.win = win
-      self.cameraOn = 1
-      self.labeler = Label(self.win, width=150, height=100, bg='black')
-
-      self.cap = cv2.VideoCapture(0)
-      self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 100)
-      self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 200)
-
-
+      self.labeler = Label(self.win, width=150)
+      self.cap = None
       self.startFeed()
+      TurnCameraOn = Button(self.win, text="Start Video", command=self.startFeed)
+      TurnCameraOff = Button(self.win, text="Stop Video", command=self.killFeed)
+      lambda x : x; TurnCameraOn.pack(side=RIGHT), TurnCameraOff.pack(side=RIGHT)
       self.labeler.pack()
-
-
-   def startFeed(self):
+   def show_feed(self):
       wCam, hCam = pyautogui.size()[0], pyautogui.size()[1]
       mpHands = mp.solutions.hands
       hands = mpHands.Hands()
       mpdraw = mp.solutions.drawing_utils
+      if self.live:
+         _, frame = self.cap.read()
+         if _:
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(cv2image)
+            results = hands.process(cv2image)
 
-      _, frame = self.cap.read()
-      cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            if results.multi_hand_landmarks:
+               if keyboard.is_pressed('Ctrl'):
+                  exit()
 
-      img = Image.fromarray(cv2image)
-      #imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-      results = hands.process(cv2image)
+               xl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * wCam
+               yl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * hCam
+               print(xl, yl)
+               pyautogui.moveTo(xl, yl)
+            # Convert image to PhotoImage
+            imgtk = ImageTk.PhotoImage(image=img)
+            self.labeler.imgtk = imgtk
+            self.labeler.configure(image=imgtk)
+            # Repeat after an interval to capture continiously
+         self.labeler.after(20, self.show_feed)
+   def startFeed(self):
+         self.live, self.cap
+         self.killFeed()
+         self.live = True
+         self.cap = cv2.VideoCapture(0)
+         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 100)
+         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 200)
+         self.show_feed()
+   def killFeed(self):
+      self.live
+      self.live = False
+      if self.cap:
+         self.cap.release()
 
-      if results.multi_hand_landmarks:
-         if keyboard.is_pressed('Ctrl'):
-            exit()
-
-         xl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * wCam
-         yl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * hCam
-         print(xl, yl)
-         pyautogui.moveTo(xl, yl)
-      # Convert image to PhotoImage
-      imgtk = ImageTk.PhotoImage(image=img)
-      self.labeler.imgtk = imgtk
-      self.labeler.configure(image=imgtk)
-      # Repeat after an interval to capture continiously
-      self.labeler.after(20, self.startFeed)
-
-   def setCamera(self):
-      pass
-   def getCamera(self):
-      pass
-   def readFeed(self):
-      return 0
 
 instance = Interface()
 
