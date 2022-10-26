c6bc710ef70c138bf6bd59a6c7ce315fee6025b1
TRECAM06 <cameron7640@live.missouristate.edu>
2022-10-26 14:11:33 -0500

    Fixed a memory leak. You may ask, there was a memory leak? Not my fault, googles fault, had to make the code even uglier to fix the leak.

--- Interface/interface.py
+++ Interface/interface.py
@@ -10,11 +10,12 @@ import pyautogui
 import tkinter.ttk as ttk
 from tkinter.ttk import *
 from ttkthemes import ThemedTk
+from memory_profiler import profile
 class Interface():
    def __init__(self):
       self.win = Tk()
       s = ttk.Style()
-      print(s.theme_names())
+
       s.theme_use('xpnative')
       self.win.title("Gesture Interface")
       self.win.wm_attributes('-toolwindow', 'True')
@@ -33,12 +34,11 @@ class Operations():
       #tkinter.Button(self.win, text="Calibrate").pack()
       print("A")
 
-   def turnOffHand(self):
-      pass
-
-
 class opticsControl():
+   @profile
    def __init__(self, win):
+      self.mpHands = mp.solutions.hands
+      self.hands = self.mpHands.Hands()
       self.live = 1
       self.handtracking = True
       self.win = win
@@ -46,47 +46,50 @@ class opticsControl():
       self.labeler = Label(self.win, width=150)
       self.cap = None
       self.startFeed()
+
       TurnCameraOn = Button(self.win, text="Start Video", command=self.startFeed)
       TurnCameraOff = Button(self.win, text="Stop Video", command=self.killFeed)
       lambda x : x; TurnCameraOn.pack(side=RIGHT), TurnCameraOff.pack(side=RIGHT), self.HandTracking.pack(side = RIGHT)
       self.labeler.pack()
 
-      #self.nxtlb.pack()
-   def show_feed(self):
-      wCam, hCam = pyautogui.size()[0], pyautogui.size()[1]
-      mpHands = mp.solutions.hands
-      hands = mpHands.Hands()
-      mpdraw = mp.solutions.drawing_utils
-      if self.live:
-         _, frame = self.cap.read()
-         frame = cv2.flip(frame, 1)
-         if _:
-            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            img = Image.fromarray(cv2image)
-            results = hands.process(cv2image)
-            if self.handtracking:
-               if results.multi_hand_landmarks:
-                  if keyboard.is_pressed('Ctrl'):
-                     exit()
 
-                  xl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].x * wCam
-                  yl = results.multi_hand_landmarks[0].landmark[mpHands.HandLandmark.INDEX_FINGER_TIP].y * hCam
-                  print(xl, yl)
-                  pyautogui.moveTo(xl, yl)
-            # Convert image to PhotoImage
-            imgtk = ImageTk.PhotoImage(image=img)
-            self.labeler.imgtk = imgtk
-            self.labeler.configure(image=imgtk)
-            # Repeat after an interval to capture continiously
-         self.labeler.after(20, self.show_feed)
+
+   def trackHands(self,frame):
+      wCam, hCam = pyautogui.size()[0], pyautogui.size()[1]
+
+
+      if self.handtracking:
+            # mpdraw = mp.solutions.drawing_utils
+            cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = self.hands.process(cv2image)
+            if results.multi_hand_landmarks:
+               xl = results.multi_hand_landmarks[0].landmark[self.mpHands.HandLandmark.INDEX_FINGER_TIP].x * wCam
+               yl = results.multi_hand_landmarks[0].landmark[self.mpHands.HandLandmark.INDEX_FINGER_TIP].y * hCam
+               pyautogui.moveTo(xl, yl)
+               results = cv2image = 0
+
+   def show_feed(self):
+            if self.live:
+               _, frame = self.cap.read()
+               frame = cv2.flip(frame, 1)
+               cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+               img = Image.fromarray(cv2image)
+               if _:
+                  opticsControl.trackHands(self,frame)
+
+               imgtk = ImageTk.PhotoImage(image=img)
+               self.labeler.imgtk = imgtk
+               self.labeler.configure(image=imgtk)
+            self.labeler.after(20, self.show_feed)
    def startFeed(self):
-         self.live, self.cap
+
          self.killFeed()
          self.live = True
          self.cap = cv2.VideoCapture(0)
 
          self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 100)
          self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 200)
+         print("Start Feed Works")
          self.show_feed()
    def killFeed(self):
       self.live
