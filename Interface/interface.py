1d54d338f902100845de4d17cf0dbaadd4b47192
TRECAM06 <cameron7640@live.missouristate.edu>
2022-10-31 15:24:27 -0500

    Added help window. Will populate with gestures soon.

--- Interface/interface.py
+++ Interface/interface.py
@@ -11,6 +11,9 @@ import tkinter.ttk as ttk
 from tkinter.ttk import *
 from ttkthemes import ThemedTk
 from memory_profiler import profile
+import numpy as np
+pyautogui.PAUSE=0.001
+
 class Interface():
    def __init__(self):
       self.win = Tk()
@@ -29,11 +32,13 @@ class Operations():
    def __init__(self, win):
       self.win = win
 
-      tkinter.Button(self.win, text="Calibrate", command=Operations.calibrate).pack()
-   def calibrate():
+   def scrollUp():
+      pyautogui.vscroll(10)
       #tkinter.Button(self.win, text="Calibrate").pack()
-      print("A")
-
+   def scrollDown():
+      pyautogui.vscroll(-10)
+   def leftClick():
+      pyautogui.leftClick()
 class opticsControl():
    @profile
    def __init__(self, win):
@@ -46,13 +51,13 @@ class opticsControl():
       self.labeler = Label(self.win, width=150)
       self.cap = None
       self.startFeed()
+      self.win2 = False
 
       TurnCameraOn = Button(self.win, text="Start Video", command=self.startFeed)
       TurnCameraOff = Button(self.win, text="Stop Video", command=self.killFeed)
-      lambda x : x; TurnCameraOn.pack(side=RIGHT), TurnCameraOff.pack(side=RIGHT), self.HandTracking.pack(side = RIGHT)
+      self.Help = Button(self.win, text="Help", command=self.helpwindow)
+      lambda x : x; TurnCameraOn.pack(side=RIGHT), TurnCameraOff.pack(side=RIGHT), self.HandTracking.pack(side = RIGHT), self.Help.pack(side = TOP)
       self.labeler.pack()
-
-
 
    def trackHands(self,frame):
       wCam, hCam = pyautogui.size()[0], pyautogui.size()[1]
@@ -62,12 +67,22 @@ class opticsControl():
             # mpdraw = mp.solutions.drawing_utils
             cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             results = self.hands.process(cv2image)
+            landmark_array = np.empty((0, 2), int)
             if results.multi_hand_landmarks:
                xl = results.multi_hand_landmarks[0].landmark[self.mpHands.HandLandmark.INDEX_FINGER_TIP].x * wCam
                yl = results.multi_hand_landmarks[0].landmark[self.mpHands.HandLandmark.INDEX_FINGER_TIP].y * hCam
-               pyautogui.moveTo(xl, yl)
-               results = cv2image = 0
+               #pyautogui.moveTo(xl, yl)
+               print(results.multi_hand_landmarks[0])
 
+
+   def helpwindow(self):
+      if self.win2 == False:
+
+         self.hw = help(self.win.winfo_x(), self.win.winfo_y(),self.win.winfo_width(), self.win.winfo_height())
+         self.win2 = True
+      else:
+         help.close_help(self.hw)
+         self.win2 = False
    def show_feed(self):
             if self.live:
                _, frame = self.cap.read()
@@ -80,6 +95,7 @@ class opticsControl():
                imgtk = ImageTk.PhotoImage(image=img)
                self.labeler.imgtk = imgtk
                self.labeler.configure(image=imgtk)
+
             self.labeler.after(20, self.show_feed)
    def startFeed(self):
 
@@ -109,5 +125,18 @@ class opticsControl():
          #self.nxtlb.config(text="The Switch is On")
          self.handtracking = True
 
+class help(tkinter.Frame):
+   def __init__(self, x, y, xoffset, yoffset):
+      helpwindow = tkinter.Frame.__init__(self)
+      helpwindow = tkinter.Toplevel(self)
+      helpwindow.wm_attributes('-toolwindow', 'True')
+      helpwindow.wm_attributes('-topmost', 'True')
+      width, height = 650, 200
+
+      helpwindow.geometry('%dx%d+%d+%d' % (width, yoffset, x+xoffset+2, y))
+      helpwindow.title("Help")
+
+   def close_help(self):
+      self.destroy()
 instance = Interface()
 
